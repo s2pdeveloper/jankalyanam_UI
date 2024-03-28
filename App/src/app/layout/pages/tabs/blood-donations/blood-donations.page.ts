be051a7d0712cf688dc98406b99d11f3b424b656
ketki pande <ketkipande09@gmail.com>
2024-03-28 17:10:29 +0530

    donation binding added by ketki

--- App/src/app/layout/pages/tabs/blood-donations/blood-donations.page.ts
+++ App/src/app/layout/pages/tabs/blood-donations/blood-donations.page.ts
@@ -1,27 +1,56 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { ModalService } from "src/app/service/modal.service";
-import { DonationDetailsComponent } from "src/app/shared/models/donation-details/donation-details.component";
-import { DonationHistoryComponent } from "src/app/shared/models/donation-history/donation-history.component";
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalService } from 'src/app/service/modal.service';
+import { DonationDetailsComponent } from 'src/app/shared/models/donation-details/donation-details.component';
+import { BloodRequestService } from 'src/app/service/request/request.service';
+import { forkJoin } from 'rxjs';
+import { StorageService } from 'src/app/core/services';
+import { DonationHistoryComponent } from 'src/app/shared/models/donation-history/donation-history.component';
+import { LoaderService } from 'src/app/core/services/loader.service';
 @Component({
-  selector: "app-blood-donations",
-  templateUrl: "./blood-donations.page.html",
-  styleUrls: ["./blood-donations.page.scss"],
+  selector: 'app-blood-donations',
+  templateUrl: './blood-donations.page.html',
+  styleUrls: ['./blood-donations.page.scss'],
 })
+
 export class BloodDonationsPage implements OnInit {
-  constructor(private router: Router, private modalService: ModalService) {}
+
+  activeSegment = 'latest';
+  currentTitle = 'history';
+  historyTabDetails: any = [];
+  latestTabDetails: any = [];
+  user: any = {};
+
+  constructor( private router: Router,
+    private modalService: ModalService,
+    private service: BloodRequestService,
+    private storage: StorageService,
+    private spinner: LoaderService) {}
 
   ngOnInit() {
-    console.log("qqqqqqqqqqqqqqqqq");
+    this.user = this.storage.get('user');
+  if (this.user.role == 'ATTENDER') {
+    this.getAllAttenderList();
+  }
     
   }
   navigateTo(url: string) {
     console.log(url);
     this.router.navigate([url]);
   }
-  activeSegment = "latest";
-  currentTitle = "history";
+  
+  async getAllAttenderList() {
+    await this.spinner.showLoader();
+    forkJoin([
+      this.service.getAllAttenderList('HISTORY'),
+      this.service.getAllAttenderList('ACTIVE'),
+    ]).subscribe(async (res) => {
+      this.historyTabDetails = res[0];
+      this.latestTabDetails = res[1];
+      await this.spinner.hideLoader();
+    });
+  }
+  
   openModel(key: string) {
     let data={}
     switch (key) {
@@ -31,7 +60,6 @@ export class BloodDonationsPage implements OnInit {
       case "latest":
         this.modalService.openModal(DonationDetailsComponent, { data });
         break;
-      
       default:
         break;
     }
--- App/src/app/service/request/request.service.ts
+++ App/src/app/service/request/request.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/core/services';
@@ -6,12 +7,16 @@ import { ApiService } from 'src/app/core/services';
   providedIn: 'root',
 })
 export class BloodRequestService {
-    routes: any = {
-        create: `blood-request`,
-      };
-      constructor(private http: ApiService) {}
-    
-      create(payload: any) {
-        return this.http.post(this.routes.create, payload);
-      }
+  routes: any = {
+    create: 'blood-request',
+    getAttenderListPath: (type: any) => `blood-request/attender-list/${type}`,
+  };
+  constructor(private http: ApiService) {}
+
+  create(payload: any) {
+    return this.http.post(this.routes.create, payload);
+  }
+  getAllAttenderList(type: any) {
+    return this.http.get(this.routes.getAttenderListPath(type));
+  }
 }
