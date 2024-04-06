072ca3a445ea53f19559f962ee37429642f5ad69
kunal-pounikar <kunalpounikar24@gmail.com>
2024-04-06 16:54:54 +0530

    responsive

--- website/src/app/features/donateblood/donateblood.component.ts
+++ website/src/app/features/donateblood/donateblood.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { DonateService } from 'src/app/service/donate.service';
+import { RestService } from 'src/app/service/res.service';
 
 @Component({
   selector: 'app-donateblood',
   templateUrl: './donateblood.component.html',
-  styleUrls: ['./donateblood.component.scss']
+  styleUrls: ['./donateblood.component.scss'],
 })
-export class DonatebloodComponent {
-  constructor(private donate :DonateService){}
+export class DonatebloodComponent implements OnInit {
+  states: any = [];
+  cities: any = [];
+
+  constructor(private donate: DonateService, private rest: RestService) {}
+
+  ngOnInit(): void {
+    this.states = this.rest.getStatesOfCountry('IN');
+
+    console.log(this.states);
+    
+  }
+  bloodGroup = [
+    {"name":"A+"},
+    {"name":"A-"},
+    {"name":"B+"},
+    {"name":"B-"},
+    {"name":"AB+"},
+    {"name":"AB-"},
+    {"name":"O+"},
+    {"name":"O-"}
+   ]
+
+   gender = [
+    {"type":"MALE"},
+    {"type":"FEMALE"},
+    {"type":"OTHER"} 
+   ]
 
   donateForm = new FormGroup({
     name: new FormControl(''),
@@ -20,14 +47,23 @@ export class DonatebloodComponent {
     illness: new FormControl(''),
     mobileNo: new FormControl(''),
     city: new FormControl(''),
-    state: new FormControl('')
+    state: new FormControl(''),
+    isWebsite: new FormControl(true),
   });
 
-  submit(){
-    console.log("this.donateForm.value",this.donateForm.value);
-    this.donate.post(this.donateForm.value).subscribe(success => {
-      console.log("success of donate ",success);
+  submit() {
+    console.log('this.donateForm.value', this.donateForm.value);
+    this.donate.post(this.donateForm.value).subscribe((success) => {
+      console.log('success of donate ', success);
       this.donateForm.reset();
-    })
+    });
+  }
+
+  getCity(state: any) {
+    console.log( state.target.value);
+    this.cities = this.rest.getCitiesOfState(
+      'IN',
+      state.target.value
+    );
   }
 }
