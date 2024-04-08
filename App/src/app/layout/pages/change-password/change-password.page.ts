40daa258ade92e6a7fd41673bf0bf10b03838773
Paritosh <paritoshbangadkar@gmail.com>
2024-04-08 18:29:35 +0530

    changes

--- App/src/app/layout/pages/change-password/change-password.page.ts
+++ App/src/app/layout/pages/change-password/change-password.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from "@ionic/angular";
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/service/auth/auth.service';
+import { ToastService } from 'src/app/core/services';
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.page.html',
@@ -8,10 +10,13 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class ChangePasswordPage implements OnInit {
 
-  constructor(private modalController: ModalController) { }
-  changepasswordForm = new FormGroup({
-    newpassword: new FormControl('', [Validators.required]),
-    confirmpassword: new FormControl('', [Validators.required]),
+  constructor(private modalController: ModalController,
+    private service: AuthService,
+    private toast: ToastService,
+  ) { }
+  changePasswordForm = new FormGroup({
+    newPassword: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
    
   });
   ngOnInit() {
@@ -19,4 +24,21 @@ export class ChangePasswordPage implements OnInit {
   dismiss() {
     this.modalController.dismiss();
   }
+  onSubmit() {
+    if (this.changePasswordForm.valid) {
+      const userPayload = {
+        newPassword: this.changePasswordForm.value.newPassword,
+        password: this.changePasswordForm.value.password
+      };
+      this.service.setPassword(userPayload).subscribe(
+        (response) => {
+          this.toast.successToast('Profile PasswordUpdated Successfully');
+        },
+        (error) => {
+          this.toast.successToast('Cant update-profile Successfully');
+        }
+      );
+    } 
+  }
+
 }
--- App/src/app/layout/pages/profile/profile.page.ts
+++ App/src/app/layout/pages/profile/profile.page.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-
+import { StorageService } from 'src/app/core/services';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/service/auth/auth.service';
+import { ToastService } from 'src/app/core/services';
+import { LoaderService } from 'src/app/core/services/loader.service';
+import { ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
-  constructor() { }
+  user: any;
+  userData: any;
+  constructor(
+    private storage: StorageService,
+    private router: Router,
+    private service: AuthService,
+    private toast: ToastService,
+    private spinner: LoaderService,
+    private toastService: ToastService,
+    private modalController: ModalController
+  ) {}
 
   ngOnInit() {
+    this.userData = this.storage.get('user');
+    if (this.userData.id) {
+      this.getByIdData(this.userData.id);
+    }
+  }
+  registrationForm = new FormGroup({
+    id: new FormControl(''),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    mobileNo: new FormControl('', [Validators.required]),
+    state: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+    role: new FormControl('', [Validators.required]),
+    status: new FormControl('', [Validators.required]),
+  });
+
+  get f() {
+    return this.registrationForm.controls;
   }
 
+  getByIdData(id) {
+    this.service.profile(id).subscribe((success: any) => {
+      this.registrationForm.patchValue(success);
+    });
+  }
+  update() {
+    let formData = this.registrationForm.value;
+    this.service.updateUser(formData.id, formData).subscribe((success: any) => {
+      this.toast.successToast('Profile Updated Successfully');
+    });
+  }
 }
