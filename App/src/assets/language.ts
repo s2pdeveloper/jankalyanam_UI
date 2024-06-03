8af2c367f13eec0cf84b8b587064e6e717b22ce6
anushka <anushkakalbande@s2pedutech.com>
2024-06-03 18:09:20 +0530

    translate

--- App/src/assets/language.ts
+++ App/src/assets/language.ts
@@ -10,7 +10,7 @@ export const languageConst={
       "illness": "illness",
       "mobileNo": "Mobile No",
       "Name": "Name",
-      "state": "state",
+      "state": "State",
       "donate" : "Donate Blood",
       "submit" : "Submit",
       "select gender" : "Select gender",
@@ -22,7 +22,40 @@ export const languageConst={
       "Tahsil": "Tahsil",
       "Village": "Village",
       "bloodgroup":"Blood Group",
-      "DOB":"DOB"
+      "DOB":"DOB",
+      "anyIllness":"Do you have any type of illness",
+      "line1":"  I agree to the",
+      "line2":"Terms and Condition",
+      "line3":"of Donation",
+      "acceptContinue":"Accept & Continue",
+      "selectUnits":"Select Units",
+      "seleteBloodType":"Select Blood Type",
+      "hospitalName":"Hospital Name",
+      "RequireDate":"Blood Require Date",
+      "bloodDonation":"Blood Donations",
+      "bloodRequest":"Blood Requests",
+      "home":"Home",
+      "donateBlood":"Donate Blood",
+      "requestBlood":"Request Blood",
+      "latest" : "Latest",
+      "history" : "History",
+      "myList":"My List",
+      "active":"Active",
+      "adminHistory":"History Admin",
+      "applyFilter":'Apply Filter',
+      "cancel":"Cancel",
+      "confirm": "Confirm",
+      "clear":"Clear",
+      "enterHospitalName":"Enter Hospital Name Here",
+      "bloodBankName" : "Blood Bank Name",
+      "enterBloodBankName":"Enter Blood Bank Name Here"
+
+
+
+
+
+
+      
 
 
     },
@@ -44,6 +77,45 @@ export const languageConst={
       "select bloodgroup" : "रक्त समूह चुनें",
       "close" : "बंद करें",
       "calendar" : "पंचांग",
-      "save" : "जमा करें"
+      "save" : "जमा करें",
+      "District": "ज़िला",
+      "Tahsil": "तहसील",
+      "Village": "गाँव",
+      "anyIllness":"क्या आपको किसी प्रकार की बीमारी है",
+      "line1":"मैं दान के ",
+      "line2":"नियमों और शर्तों",
+      "line3":"से सहमत हूं",
+      "acceptContinue":"स्वीकार करें और जारी रखें",
+      "selectUnits":"इकाइयों का चयन करें",
+      "seleteBloodType":"रक्त प्रकार का चयन करें",
+      "hospitalName":"अस्पताल का नाम",
+      "RequireDate":"रक्त की आवश्यकता की तारीख",
+      "bloodDonation":"रक्तदान",
+      "bloodRequest" : "रक्त अनुरोध",
+      "home":"घर",
+      "donateBlood":"रक्त दान करें",
+      "requestBlood":"रक्त का अनुरोध करें",
+      "latest" : "नवीनतम",
+      "history" : "इतिहास",
+      "myList" : "मेरी सूची",
+      "active":"सक्रिय",
+      "adminHistory":"प्रशासन इतिहास",
+      "applyFilter":'फिल्टर लागू करें',
+      "cancel":'रद्द करें',
+      "confirm": " पुष्टि करें",
+      "clear":"साफ़ करें",
+      "enterHospitalName":"यहां अस्पताल का नाम दर्ज करें",
+      "bloodBankName" : "ब्लड बैंक का नाम",
+      "enterBloodBankName":"यहां ब्लड बैंक का नाम दर्ज करें",
+      "bloodDonationDate" : 'रक्त दान दिनांक'
+
+
+
+
+
+
+
+
+
     }
 }
